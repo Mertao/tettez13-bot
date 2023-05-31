cb9603fb1c3f8118cfd884ac4112ec621d3a8e74
Mertao <artem.olleshev@gmail.com>
2023-05-31 09:05:33 +0300

    first

---
+++ handlers/user_handlers.py
@@ -0,0 +1,140 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Text
+from keyboards.keyboards import (main_menu_kb, comp_services_kb,
+                                 wildberries_menu_kb, inst_menu_kb)
+from lexicon.lexicon_ru import LEXICON_RU
+# from services.services import ...
+
+
+# инициализируем роутер
+router: Router = Router()
+
+
+# хэндлер для "/start"
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'].format(
+        message.from_user.full_name), reply_markup=main_menu_kb)
+
+
+# хэндлер для "Вопросы"
+@router.message(Text(text=LEXICON_RU['add_questions']))
+async def process_add_questions_answer(message: Message):
+    await message.answer(text=LEXICON_RU['add_questions_answer'],
+                         reply_markup=main_menu_kb)
+
+
+# хэндлер для "О нас"
+@router.message(Text(text=LEXICON_RU['about_us']))
+async def process_about_us_answer(message: Message):
+    await message.answer(text=LEXICON_RU['about_us_answer'],
+                         reply_markup=main_menu_kb)
+
+
+# хэндлер для "Дополнительный прайс"
+@router.message(Text(text=LEXICON_RU['additional_price']))
+async def process_add_price_answer(message: Message):
+    await message.answer(text=LEXICON_RU['additional_price_answer'],
+                         reply_markup=main_menu_kb)
+
+
+# хэндлер для "Дополнительные услуги"
+@router.message(Text(text=LEXICON_RU['comp_services']))
+async def process_comp_services_answer(message: Message):
+    await message.answer(text=LEXICON_RU['comp_services'],
+                         reply_markup=comp_services_kb)
+
+
+# хэндлер для "Назад"
+@router.message(Text(text=LEXICON_RU['back_button']))
+async def process_back_answer(message: Message):
+    await message.answer(text=LEXICON_RU['back_button_answer'],
+                         reply_markup=main_menu_kb)
+
+
+# хэндлер для "Брендбук"
+@router.message(Text(text=LEXICON_RU['comp_services_brandbook_button']))
+async def process_comp_services_brandbook_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['comp_services_brandbook_answer'])
+
+
+# хэндлер для "Логотип"
+@router.message(Text(text=LEXICON_RU['comp_services_logo_button']))
+async def process_comp_services_logo_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['comp_services_logo_answer'])
+
+
+# хэндлер для "Стилизация образа"
+@router.message(Text(text=LEXICON_RU['comp_services_style_button']))
+async def process_comp_services_style_answer(message: Message):
+    await message.answer(text=LEXICON_RU['comp_services_style_answer'])
+
+
+# хэндлер для "Съемка"
+@router.message(Text(text=LEXICON_RU['comp_services_shooting_button']))
+async def process_comp_services_shooting_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['comp_services_shooting_answer'])
+
+
+# хэндлер для "Съемка лукбука"
+@router.message(Text(text=LEXICON_RU['comp_services_shooting_lb_button']))
+async def process_comp_services_shooting_lb_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['comp_services_shooting_lb_answer'])
+
+
+# хэндлер для "Вайлдберрис"
+@router.message(Text(text=LEXICON_RU['button_wb']))
+async def process_button_wb_answer(message: Message):
+    await message.answer(text=LEXICON_RU['button_wb'],
+                         reply_markup=wildberries_menu_kb)
+
+
+# хэндлер для "Инфографика"
+@router.message(Text(text=LEXICON_RU['wildberries_infographics_button']))
+async def process_wildberries_infographics_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['wildberries_infographics_answer'])
+
+
+# хэндлер для "Копирайтинг"
+@router.message(Text(text=LEXICON_RU['wildberries_copywriting_button']))
+async def process_wildberries_copywriting_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['wildberries_copywriting_answer'])
+
+
+# хэндлер для "Инстаграм"
+@router.message(Text(text=LEXICON_RU['button_inst']))
+async def process_button_inst_answer(message: Message):
+    await message.answer(text=LEXICON_RU['button_inst'],
+                         reply_markup=inst_menu_kb)
+
+
+# хэндлер для "Полное ведение"
+@router.message(Text(text=LEXICON_RU['inst_full_maintenance_button']))
+async def process_inst_full_maintenance_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['inst_full_maintenance_answer'])
+
+
+# хэндлер для "Контент-съемка"
+@router.message(Text(text=LEXICON_RU['inst_content_shooting_button']))
+async def process_inst_content_shooting_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['inst_content_shooting_answer'])
+
+
+# хэндлер для "Визуал"
+@router.message(Text(text=LEXICON_RU['inst_visual_button']))
+async def process_inst_visual_button_anwer(message: Message):
+    await message.answer(text=LEXICON_RU['inst_visual_answer'])
+
+
+# хэндлер для "Reels"
+@router.message(Text(text=LEXICON_RU['inst_reels_button']))
+async def process_inst_reels_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['inst_reels_answer_one'])
+    await message.answer(text=LEXICON_RU['inst_reels_answer_more'])
+
+
+# хэндлер для "Графика"
+@router.message(Text(text=LEXICON_RU['inst_graphics_button']))
+async def process_inst_graphics_button_answer(message: Message):
+    await message.answer(text=LEXICON_RU['inst_graphics_answer'])
---
+++ keyboards/keyboards.py
@@ -0,0 +1,105 @@
+from aiogram.types import KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+from lexicon.lexicon_ru import LEXICON_RU
+
+
+# ------------- main_menu клавиатура -------------
+# Кнопки для main_menu
+button_inst: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_inst'])
+button_wb: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_wb'])
+comp_services_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['comp_services'])
+additional_price_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['additional_price'])
+about_us_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['about_us'])
+add_questions_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['add_questions'])
+
+# инициализируем билдер для клавиатуры main_menu
+main_menu_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+# добавляем кнопки в билдер
+main_menu_builder.add(button_inst, button_wb, comp_services_button,
+                      additional_price_button, about_us_button,
+                      add_questions_button)
+# выравниваем клавиатуру
+main_menu_builder.adjust(2, 2, 2)
+# создаем клавиатуру
+main_menu_kb = main_menu_builder.as_markup(resize_keyboard=True)
+# ------------- main_menu клавиатура -------------
+
+
+# ------------- comp_services клавиатура -------------
+# кнопки для comp_services_menu
+comp_services_brandbook_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['comp_services_brandbook_button'])
+comp_services_logo_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['comp_services_logo_button'])
+comp_services_style_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['comp_services_style_button'])
+comp_services_shooting_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['comp_services_shooting_button'])
+comp_services_shooting_lb_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['comp_services_shooting_lb_button'])
+back_button: KeyboardButton = KeyboardButton(text=LEXICON_RU['back_button'])
+
+# инициализируем билдер для клавиатуры comp_services_menu
+comp_services_menu_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+# добавляем кнопки в билдер
+comp_services_menu_builder.row(comp_services_brandbook_button,
+                               comp_services_logo_button,
+                               comp_services_style_button,
+                               comp_services_shooting_button,
+                               comp_services_shooting_lb_button,
+                               back_button, width=1)
+# создаем клавиатуру
+comp_services_kb = comp_services_menu_builder.as_markup(resize_keyboard=True)
+
+# ------------- comp_services клавиатура -------------
+
+
+# ------------- wildberries клавиатура -------------
+
+wildberries_infographics_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['wildberries_infographics_button'])
+wildberries_copywriting_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['wildberries_copywriting_button'])
+wildberries_comp_services_shooting_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['wildberries_comp_services_shooting_button'])
+back_button: KeyboardButton = KeyboardButton(text=LEXICON_RU['back_button'])
+
+wildberries_menu_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+
+wildberries_menu_builder.row(wildberries_infographics_button,
+                             wildberries_copywriting_button,
+                             wildberries_comp_services_shooting_button,
+                             back_button, width=1)
+
+wildberries_menu_kb = wildberries_menu_builder.as_markup(resize_keyboard=True)
+
+# ------------- wildberries клавиатура -------------
+
+
+# ------------- inst клавиатура -------------
+inst_full_maintenance_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['inst_full_maintenance_button'])
+inst_content_shooting_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['inst_content_shooting_button'])
+inst_visual_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['inst_visual_button'])
+inst_reels_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['inst_reels_button'])
+inst_graphics_button: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['inst_graphics_button'])
+
+inst_menu_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+
+inst_menu_kb_builder.row(inst_full_maintenance_button,
+                         inst_content_shooting_button,
+                         inst_visual_button, inst_reels_button,
+                         inst_graphics_button, back_button, width=1)
+
+inst_menu_kb = inst_menu_kb_builder.as_markup(resize_keyboard=True)
+
+# ------------- inst клавиатура -------------
